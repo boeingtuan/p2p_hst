2986daa6e0ec8c95bcd98f55ae9be9a95158177e
boeingtuan <boeingtuan@outlook.com>
2015-10-13 19:20:55 +0700

    Update ChatCloseInfo

--- src/CentralPoint/ChatCloseInfo.java
+++ src/CentralPoint/ChatCloseInfo.java
@@ -1,15 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package CentralPoint;
 
-/**
- *
- * @author Tuân
- */
 public class ChatCloseInfo {
+    private boolean isChatClose;
+
+    public ChatCloseInfo(boolean isChatClose) {
+        this.isChatClose = isChatClose;
+    }
+    
+    public void setIsChatClose(boolean isChatClose) {
+        this.isChatClose = isChatClose;
+    }
+    
+    public boolean getIsChatClose() {
+        return this.isChatClose;
+    }
     
 }
