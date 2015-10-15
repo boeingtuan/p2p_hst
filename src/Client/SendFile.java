dffb3e67b815acdd99dfb83e1ecd9084718a4e68
sonnguyevuson <sonnguyevuson@gmail.com>
2015-10-15 10:48:53 +0700

    Add 2 file to implement sending and receiving File

--- src/Client/SendFile.java
+++ src/Client/SendFile.java
@@ -6,10 +6,24 @@
 
 package Client;
 
+import java.net.Socket;
+
 /**
  *
  * @author Tuân
  */
 public class SendFile {
     
+    private Socket socket;
+    private String filepath; // filepath: duong dan toi file can gui. VD: C:\Users\sonng_000\Documents\abc.txt
+    
+    public SendFile(Socket socket, String filepath) {
+        this.socket = socket;
+        this.filepath = filepath;
+    }
+    
+    public void sendFile() { 
+        // hien thuc gui file qua socket, gia su socket da duoc ket noi
+        
+    }
 }
